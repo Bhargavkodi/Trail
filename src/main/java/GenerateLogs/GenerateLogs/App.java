ecc6bd53ddc0979395f747ebb9c7eccf1fa56800
ethirajp <ethirajp@172.16.3.40>
2018-10-04 18:26:27 +0530

    added space

--- src/main/java/GenerateLogs/GenerateLogs/App.java
+++ src/main/java/GenerateLogs/GenerateLogs/App.java
@@ -3,7 +3,7 @@ package GenerateLogs.GenerateLogs;
 class App{
   public void run()
 {
-System.out.println("Work hard. Have fun. Make history");
+System.out.println("Work hard. Have fun. Make history ");
 }
   }
   class B extends App{
